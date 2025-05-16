73b83ee9282ac1e5204c95a3ade63be3527e23ee
Yuchen Huang <YuchenHuang97@Gmail.com>
2025-05-16 10:14:09 -0400

    Update Theme.tsx

--- app/components/Theme.tsx
+++ app/components/Theme.tsx
@@ -1,4 +1,7 @@
 import { createTheme } from "@mui/material";
+import "@fontsource/poppins"; // Defaults to weight 400
+import "@fontsource/poppins/400.css"; // Specify weight
+import "@fontsource/poppins/400-italic.css"; // Specify weight and style
 
 const theme = createTheme({
     palette: {
